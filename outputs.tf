684797a933cab598d6f068a5896a0f4caf7ed7bd
Michael Reiger <michael@rauschpfeife.net>
2021-09-29 22:50:40 +0200

    Last loadbalancer IP reference...

--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,6 @@ output "k3s-api-ip" {
   value = module.free-tier-k3s.k3s-api-ip
 }
 
-output "loadbalaner-ip" {
-  value = "http://${module.free-tier-k3s.loadbalacer_ip}"
-}
+# output "loadbalaner-ip" {
+#   value = "http://${module.free-tier-k3s.loadbalacer_ip}"
+# }
